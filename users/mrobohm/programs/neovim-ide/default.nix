469845f970879df6ab026042331e14c2128f929d
hafflationist <internetzverbindung(an)gmail(punkt)com>
2023-04-25 22:36:06 +0200

    feat: neovim config

---
+++ users/mrobohm/programs/neovim-ide/default.nix
@@ -0,0 +1,137 @@
+{ lib, pkgs, ... }:
+
+let
+  metals = pkgs.callPackage ./metals.nix { };
+in
+{
+  programs.neovim = {
+    enable = true;
+    settings = {
+      vim = {
+        viAlias = false;
+        vimAlias = true;
+        preventJunkFiles = true;
+        customPlugins = with pkgs.vimPlugins; [
+          multiple-cursors
+          vim-mergetool
+          vim-repeat
+        ];
+        neovim.package = pkgs.neovim-nightly;
+        lsp = {
+          enable = true;
+          folds = true;
+          formatOnSave = false;
+          lightbulb.enable = true;
+          lspsaga.enable = false;
+          nvimCodeActionMenu.enable = true;
+          trouble.enable = true;
+          lspSignature.enable = true;
+          nix = {
+            enable = true;
+            type = "nil";
+          };
+          scala = {
+            inherit metals;
+            enable = true;
+          };
+          ts = true;
+          smithy.enable = true;
+          rust.enable = false;
+          dhall = true;
+          elm = true;
+          haskell = false;
+          sql = false;
+          python = false;
+          clang = false;
+          go = false;
+        };
+        plantuml.enable = true;
+        fx.automaton.enable = true;
+        visuals = {
+          enable = true;
+          nvimWebDevicons.enable = true;
+          lspkind.enable = true;
+          indentBlankline = {
+            enable = true;
+            fillChar = "";
+            eolChar = "";
+            showCurrContext = true;
+          };
+          cursorWordline = {
+            enable = true;
+            lineTimeout = 0;
+          };
+        };
+        statusline.lualine = {
+          enable = true;
+          theme = "onedark";
+        };
+        theme = {
+          enable = true;
+          name = "onedark";
+          style = "deep";
+          transparency = true;
+        };
+        autopairs.enable = true;
+        autocomplete = {
+          enable = true;
+          type = "nvim-cmp";
+        };
+        filetree.nvimTreeLua = {
+          enable = true;
+          hideDotFiles = false;
+          hideFiles = [ "node_modules" ".cache" ];
+          openOnSetup = false;
+        };
+        neoclip.enable = true;
+        dial.enable = true;
+        hop.enable = true;
+        notifications.enable = true;
+        todo.enable = true;
+        tabline.nvimBufferline.enable = true;
+        treesitter = {
+          enable = true;
+          autotagHtml = true;
+          context.enable = true;
+        };
+        keys = {
+          enable = true;
+          whichKey.enable = true;
+        };
+        comments = {
+          enable = true;
+          type = "nerdcommenter";
+        };
+        shortcuts = {
+          enable = true;
+        };
+        surround = {
+          enable = true;
+        };
+        telescope = {
+          enable = true;
+        };
+        markdown = {
+          enable = true;
+          glow.enable = true;
+        };
+        git = {
+          enable = true;
+          gitsigns.enable = true;
+        };
+        spider = {
+          enable = false;
+          skipInsignificantPunctuation = true;
+        };
+        mind = {
+          enable = true;
+          # Documents dir is synced to the cloud
+          persistence = {
+            dataDir = "~/Documents/mind.nvim/data";
+            statePath = "~/Documents/mind.nvim/mind.json";
+          };
+        };
+      };
+    };
+  };
+}
---
+++ users/mrobohm/programs/neovim-ide/metals.nix
@@ -0,0 +1,6 @@
+{ metalsBuilder }:
+
+metalsBuilder {
+  version = "0.11.11+86-18358302-SNAPSHOT";
+  outputHash = "sha256-2uZC3MiaMeV18XDhtLsUm2fAXe6SqNw9CC5RXBsGdjo=";
+}
