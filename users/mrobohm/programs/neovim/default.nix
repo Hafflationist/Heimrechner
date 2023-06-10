6e38c38ee0645b07e9ebd3a886c3addcd207a3ec
hafflationist <internetzverbindung(an)gmail(punkt)com>
2023-06-10 17:14:12 +0200

    feat: adds neovim pluging (telescope and peekup)

--- users/mrobohm/programs/neovim/default.nix
+++ users/mrobohm/programs/neovim/default.nix
@@ -21,8 +21,10 @@
       { plugin = nvim-navbuddy; type = "lua"; config = builtins.readFile(./pluginConfig/nvim-navbuddy.lua); }
       nvim-navic
       nvim-notify
+      { plugin = nvim-peekup; type = "lua"; config = builtins.readFile(./pluginConfig/peekup.lua); }
       { plugin = nvim-tree-lua; type = "lua"; config = builtins.readFile(./pluginConfig/tree-lua.lua); }
       suda-vim
+      { plugin = telescope-nvim; type= "lua"; config = builtins.readFile(./pluginConfig/telescope.lua); }
       vim-nix
       { plugin = vim-airline; config = "let g:airline_powerline_fonts = 1";}
       YouCompleteMe
---
+++ users/mrobohm/programs/neovim/pluginConfig/peekup.lua
@@ -0,0 +1,2 @@
+require('nvim-peekup.config').on_keystroke["paste_reg"] = "\""
+require('nvim-peekup.config').on_keystroke["delay"] = '200ms'
