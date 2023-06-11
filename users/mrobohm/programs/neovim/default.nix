b40171ebe0d51f6818bcb5443051f92f32e2fe37
hafflationist <internetzverbindung(an)gmail(punkt)com>
2023-06-11 16:18:52 +0200

    feat: neovim plugins

--- users/mrobohm/programs/neovim/default.nix
+++ users/mrobohm/programs/neovim/default.nix
@@ -12,21 +12,25 @@
       set expandtab
     ";
     plugins =  with pkgs.vimPlugins; [
+      { plugin = barbecue-nvim; type = "lua";config = "require(\"barbecue\").setup()";}
       #coc-nvim
       { plugin = context-vim; config = "let g:context_highlight_normal = 'Normal'";}
       fugitive
+      { plugin = neoscroll-nvim; type = "lua"; config = builtins.readFile(./pluginConfig/neoscroll.lua); }
       { plugin = nightfox-nvim; type = "lua"; config = builtins.readFile(./pluginConfig/nightfox-nvim.lua); }
+      { plugin = noice-nvim; type = "lua"; config = builtins.readFile(./pluginConfig/noice.lua); }
       nui-nvim
       { plugin = nvim-lspconfig; type = "lua"; config = builtins.readFile(./pluginConfig/lspconfig.lua); }
       { plugin = nvim-navbuddy; type = "lua"; config = builtins.readFile(./pluginConfig/nvim-navbuddy.lua); }
       nvim-navic
       nvim-notify
       { plugin = nvim-peekup; type = "lua"; config = builtins.readFile(./pluginConfig/peekup.lua); }
+      { plugin = nvim-scrollbar; type = "lua"; config = builtins.readFile(./pluginConfig/scrollbar.lua); }
       { plugin = nvim-tree-lua; type = "lua"; config = builtins.readFile(./pluginConfig/tree-lua.lua); }
       suda-vim
-      { plugin = telescope-nvim; type= "lua"; config = builtins.readFile(./pluginConfig/telescope.lua); }
+      { plugin = telescope-nvim; type = "lua"; config = builtins.readFile(./pluginConfig/telescope.lua); }
       vim-nix
-      { plugin = vim-airline; config = "let g:airline_powerline_fonts = 1";}
+      { plugin = feline-nvim; type = "lua"; config = builtins.readFile(./pluginConfig/airline.lua); }
       YouCompleteMe
       { plugin = vim-startify; config = "let g:startify_change_to_vcs_root = 0";}
     ];
