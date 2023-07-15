a253812cacdb5377353225cde7e0dba6d34057af
hafflationist <internetzverbindung(an)gmail(punkt)com>
2023-07-15 16:13:53 +0200

    feat: neovim tree toggle with leader-a

--- users/mrobohm/programs/neovim/pluginConfig/tree-lua.lua
+++ users/mrobohm/programs/neovim/pluginConfig/tree-lua.lua
@@ -1,4 +1,3 @@
-
 -- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
@@ -6,9 +5,15 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
+local api = require "nvim-tree.api"
+vim.keymap.set('n', '<leader>b', api.tree.toggle, {})
+
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  renderer = {
-    group_empty = true,
-  },
+    sort_by = "case_sensitive",
+    renderer = {
+        group_empty = true,
+    },
+    view = {
+        width = 48,
+    },
 })
