d2d65deaa800da6622033efe1f5f915eb36d1733
hafflationist <internetzverbindung(an)gmail(punkt)com>
2023-09-04 20:59:11 +0200

    style: changes color of lsd

--- users/mrobohm/programs/lsd/default.nix
+++ users/mrobohm/programs/lsd/default.nix
@@ -1,11 +1,13 @@
 { pkgs, lib, ... }:
 
 {
+  home.file.".config/lsd/themes/lsd-colors.yaml".source = ./lsd-colors.yaml;
   programs.lsd = {
     enable = true;
     enableAliases = true;
     settings = {
       date = "+%Y-%m-%d %H:%M:%S";
+      color.theme = "lsd-colors";
     };
   };
 }
