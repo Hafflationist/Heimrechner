e63d8ee0a4d812e7b1828cd62da438c8a431fd1d
hafflationist <internetzverbindung(an)gmail(punkt)com>
2024-05-01 12:16:26 +0200

    chore: update flake inputs

--- wm/xmonad.nix
+++ wm/xmonad.nix
@@ -2,10 +2,10 @@
 
 {
   services = {
+    displayManager.defaultSession = "none+xmonad";
     xserver = {
       enable = true;
       xkb.layout = "de";
-      displayManager.defaultSession = "none+xmonad";
       windowManager.xmonad = {
         enable = true;
         enableContribAndExtras = true;
