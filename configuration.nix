86933bf432f02f6b5eb31f17e66bdd53bada0df5
hafflationist <internetzverbindung(an)gmail(punkt)com>
2024-05-01 11:41:25 +0200

    feat: podman

--- configuration.nix
+++ configuration.nix
@@ -142,6 +142,19 @@
     nixos-bgrt-plymouth
   ];
 
+   virtualisation.containers.enable = true;
+   virtualisation = {
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
    virtualisation.virtualbox.host.enable = true;
    users.extraGroups.vboxusers.members = [ "virtualbox" ];
 
