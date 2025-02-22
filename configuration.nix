bfec1b32cac7742c153ad8113f3ef17e11e6cd7b
hafflationist <internetzverbindung(an)gmail(punkt)com>
2025-02-22 21:55:21 +0100

    chore: enalbe audio

--- configuration.nix
+++ configuration.nix
@@ -85,6 +85,7 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    audio.enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
