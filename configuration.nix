a53afc3007a4379694ab52214d247e012de8c5c6
hafflationist <internetzverbindung(an)gmail(punkt)com>
2025-02-23 22:02:13 +0100

    chore: reorganisiere SChreibtischkonfiguration

--- configuration.nix
+++ configuration.nix
@@ -8,8 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      # ./wm/xmonad.nix
-      ./wm/hyprland.nix
+      ./wm/desktop.nix
     ];
 
   # Bootloader.
