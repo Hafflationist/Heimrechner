d9191b57c72d994d69f616777a33d3c0e43ebecb
hafflationist <internetzverbindung(an)gmail(punkt)com>
2025-05-31 20:37:12 +0200

    fix: stylix-warnung

--- users/mrobohm/stylix/default.nix
+++ users/mrobohm/stylix/default.nix
@@ -1,12 +1,10 @@
-{ ... }:
-
-{
+{...}: {
   stylix = {
     enable = true;
     targets = {
       bat.enable = true;
       btop.enable = true;
-      firefox.enable = true;
+      # firefox.enable = true;
       alacritty.enable = true;
     };
     opacity.terminal = 0.25;
