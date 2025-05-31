3843d9faf7b3bc2bc5ae7f83ef132365df6fb6c3
hafflationist <internetzverbindung(an)gmail(punkt)com>
2025-05-31 20:32:52 +0200

    fix!: aktualisierung zurücksetzen wegen amdgpu-problemen

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS-Heimrechner";
 
   inputs = {
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
